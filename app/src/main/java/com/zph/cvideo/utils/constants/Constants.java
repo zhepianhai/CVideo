8bda58180de86f0456451f3d0b3260c9b95a52fa
zhepianhai <dlkzph084412120>
2018-04-27 16:26:05 +0800

    2018.4.27  网络,数据库，UI,缓存,封装完成

--- app/src/main/java/com/zph/cvideo/utils/constants/Constants.java
+++ app/src/main/java/com/zph/cvideo/utils/constants/Constants.java
@@ -9,14 +9,13 @@ package com.zph.cvideo.utils.constants;
 public class Constants {
     public static final String DB_NAME = "zph_cvideo";
 
-
-    public static final String[] HOMETABLNAMES={"图片","视频","音频"};
-    public static final int[] HOMETABLTYPES= new int[]{1001, 1002, 1003};
     public static final int HOME=1000;
     public static final int HOME_PICTURE=1001;
     public static final int HOME_VIDEO=1002;
     public static final int HOME_AUDIO=1003;
-    public static final int[] HOMETYPES={HOME_PICTURE,HOME_VIDEO,HOME_AUDIO};
+    public static final String[] HOMETABLNAMES={"图片","视频","音频"};
+    public static final int[] HOMETABLTYPES={HOME_PICTURE,HOME_VIDEO,HOME_AUDIO};
+
     public static final int INFO=2000;
     public static final int MORE=3000;
 
